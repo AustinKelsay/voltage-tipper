f6594c53189e0c12b872eb6ee13d85972ad1f64c
austinkelsay <austinkelsay@yahoo.com>
2024-06-07 13:51:20 -0500

    See original url in getlnurl

--- src/pages/api/getlnurl.js
+++ src/pages/api/getlnurl.js
@@ -1,15 +1,16 @@
 import { bech32 } from 'bech32';
 
 function encodeLnurl(url) {
-   const words = bech32.toWords(Buffer.from(url, 'utf8'));
-   return bech32.encode('lnurl', words, 2000).toUpperCase();
+    const words = bech32.toWords(Buffer.from(url, 'utf8'));
+    return bech32.encode('lnurl', words, 2000).toUpperCase();
 }
 
 export default function handler(req, res) {
-   const originalUrl = `${process.env.BACKEND_URL}/api/lnurl`
-   const encodedLnurl = encodeLnurl(originalUrl);
+    const originalUrl = `${process.env.BACKEND_URL}/api/lnurl`
+    const encodedLnurl = encodeLnurl(originalUrl);
 
-   res.status(200).json({
-       lnurl: encodedLnurl
-   })
+    res.status(200).json({
+        lnurl: encodedLnurl,
+        originalUrl: originalUrl
+    })
 }
