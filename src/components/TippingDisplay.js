e4f852d8a3fadc6f65752286308b99746026fbe3
austinkelsay <austinkelsay@yahoo.com>
2024-08-02 09:14:40 -0500

    webln to pay

--- src/components/TippingDisplay.js
+++ src/components/TippingDisplay.js
@@ -5,19 +5,36 @@ import { useToast } from "@/hooks/useToast";
 const TippingDisplay = ({ invoice, listenTimer, formatTime }) => {
     const { showToast } = useToast();
 
-    const copyToClipboard = () => {
-        if (invoice) {
-            navigator.clipboard.writeText(invoice);
+    // const copyToClipboard = () => {
+    //     if (invoice) {
+    //         navigator.clipboard.writeText(invoice);
+    //         showToast('success', 'Copied', 'Invoice copied to clipboard');
+    //     }
+    // };
+
+    const copyToClipboard = async () => {
+        try {
+            await navigator.clipboard.writeText(invoice);
+          if (window && window?.webln && window?.webln?.sendPayment) {
+            await window.webln.enable();
+            const result = await window.webln.sendPayment(invoice);
+            if (result && result?.preimage) {
+              showToast('success', 'Success', 'Invoice paid');
+            }
+          } else if (invoice) {
             showToast('success', 'Copied', 'Invoice copied to clipboard');
+          }
+        } catch (err) {
+          console.error('Failed to copy:', err);
         }
-    };
+      };
 
     return (
         <div className="flex flex-col justify-center bg-slate-600 rounded-md p-2">
             <span className="text-center text-sm">expires in</span>
             <p className="text-center text-2xl pt-0 mt-0">{formatTime(listenTimer)}</p>
-            <div className="mx-auto w-fit m-4 border-2 rounded-lg bg-white p-2 cursor-pointer hover:opacity-75" onClick={copyToClipboard}>
-                <QRCodeSVG size={350} value={invoice} />
+            <div className="mx-auto w-fit m-2 border-2 rounded-lg bg-white p-2 cursor-pointer hover:opacity-75" onClick={copyToClipboard}>
+                <QRCodeSVG size={300} value={invoice} />
                 {/* Only show first 5 chars and last 5 chars with ... in the middle */}
                 <p className="text-black text-center my-0 text-xl">{invoice.slice(0, 10) + "..." + invoice.slice(-10)}</p>
                 <p className="text-center text-xs pt-0 text-black">Click to copy</p>
